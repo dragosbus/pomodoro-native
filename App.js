a30ef86f5a4fbf1a9b440c4eadd48216359b7173
dragosbus <dragosbusuioc94@gmail.com>
2018-08-26 15:03:14 +0200

    created clock compoennt

--- App.js
+++ App.js
@@ -1,99 +1,15 @@
 import React from 'react';
-import { StyleSheet, Text, View, Alert } from 'react-native';
+import {Provider} from 'react-redux';
+import store from './src/store';
 
-import Timer from './components/Timer';
-import WorkSlider from './components/Sliders';
-import WrapperButtons from './components/Buttons';
+import Clock from './src/Clock';
 
 export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      workTimer: 0,
-      pauseTimer: 0,
-      workTimerOn: false,
-      pauseTimerOn: false
-    };
-    this.changeTimer = this.changeTimer.bind(this);
-    this.changePauseTimerHandler = this.changePauseTimerHandler.bind(this);
-    this.decrementCounter = this.decrementCounter.bind(this);
-    this.startTimer = this.startTimer.bind(this);
-    this.stopTimer = this.stopTimer.bind(this);
-    this.resetTimer = this.resetTimer.bind(this);
-  }
-
-  decrementCounter() {
-    if (this.state.workTimer > 0) {
-      this.setState({ 
-        workTimer: this.state.workTimer - 1, 
-        workTimer: true });
-    } else {
-      this.setState({
-        workTimer: false,
-        pauseTimerOn: true,
-        pauseTimer: this.state.pauseTimer - 1
-      });
-    }
-  }
-
-  changeTimer(value) {
-    this.setState({ workTimer: parseInt(value) * 60 });
-  }
-
-  changePauseTimerHandler(value) {
-    this.setState({ pauseTimer: parseInt(value) * 60 });
-  }
-
-  startTimer() {
-    this.interval = setInterval(this.decrementCounter, 1000);
-  }
-
-  stopTimer() {
-    clearInterval(this.interval);
-  }
-
-  resetTimer() {
-    this.setState({ workTimer: 0 });
-  }
-
   render() {
-    let { workTimer } = this.state;
-
-    let hour = Math.floor(workTimer / 3600);
-    let minute = Math.floor(workTimer / 60 - hour * 60);
-    let seconds = workTimer % 60;
-    //convert them to string
-    hour = hour < 10 ? `0${hour}` : `${hour}`;
-    minute = minute < 10 ? `0${minute}` : `${minute}`;
-    seconds = seconds < 10 ? `0${seconds}` : `${seconds}`;
-
-    let clock = `${hour}:${minute}:${seconds}`;
-    return (
-      <View style={styles.container}>
-        <Timer timer={clock} />
-        <WorkSlider
-          changeWorkTimerHandler={this.changeTimer}
-          changePauseTimerHandler={this.changePauseTimerHandler}
-          workTimer={this.state.workTimer}
-          pauseTimer={this.state.pauseTimer}
-          timerIsRunning={this.state.timerIsRunning}
-        />
-        <WrapperButtons
-          startBtn={this.startTimer}
-          stopBtn={this.stopTimer}
-          resetBtn={this.resetTimer}
-          workTimer={this.state.workTimer}
-        />
-      </View>
+    return(
+      <Provider store={store}>
+        <Clock/>
+      </Provider>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
-});
--- src/Clock.js
+++ src/Clock.js
@@ -2,23 +2,39 @@ import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import {connect} from 'react-redux';
 
-import Timer from './components/Timer';
-import WorkSlider from './components/Sliders';
-import WrapperButtons from './components/Buttons';
+import Timer from '../components/Timer';
+import Sliders from '../components/Sliders';
+import WrapperButtons from '../components/Buttons';
+
+const setClock = timer => {
+  let hour = Math.floor(timer / 3600);
+    let minute = Math.floor(timer / 60 - hour * 60);
+    let seconds = timer % 60;
+    //convert them to string
+    hour = hour < 10 ? `0${hour}` : `${hour}`;
+    minute = minute < 10 ? `0${minute}` : `${minute}`;
+    seconds = seconds < 10 ? `0${seconds}` : `${seconds}`;
+
+    let clock = `${hour}:${minute}:${seconds}`;
+
+    return clock;
+};
 
 class Clock extends React.Component {
 
   render() {
     let {workTimer, pauseTimer} = this.props;
+    let clock = setClock(workTimer);
+    
     return (
       <View style={styles.container}>
-        <Timer timer={clock} />
-        <WorkSlider
+        <Timer timer={clock}/>
+        <Sliders
           workTimer={workTimer}
           pauseTimer={pauseTimer}
         />
         <WrapperButtons
-          workTimer={workTimer}
+        
         />
       </View>
     );
